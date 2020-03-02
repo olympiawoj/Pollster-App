6a05607313eb0cd8e302d0fd94a4ae05683be2ca
olympia <owojcik7@gmail.com>
2020-03-02 11:50:56 -0500

    Create admin user and setup admin area

--- pollster/polls/admin.py
+++ pollster/polls/admin.py
@@ -1,3 +1,27 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from.models import Question, Choice
+
+admin.site.site_header = "Pollster Admin" 
+admin.site.site_title = "Pollster Admin Area"
+admin.site.index_title = "Welcome to the Pollster admin area"
+
+class ChoiceInline(admin.TabularInline):
+    model = Choice 
+    #how many extra fields do we want
+    extra = 3
+
+#name of model and admin
+class QuestionAdmin(admin.ModelAdmin):
+    #name, dict with fields, 
+    #fieldsets is a tuple, so we need a hanging comma
+    fieldsets = [(None, {'fields': ['question_text']}), ('Date Information', {'fields': ['pub_date'], 'classes': ['collapse']}), ]
+    inlines = [ChoiceInline]
+
+
+admin.site.register(Question, QuestionAdmin)
+
+# admin.site.register(Question)
+# admin.site.register(Choice)
