a614d2f61db84d42c8f418c1c13af2cd35129723
olympia <owojcik7@gmail.com>
2020-03-07 12:11:55 -0500

    Add details view and route for polls

--- pollster/polls/urls.py
+++ pollster/polls/urls.py
@@ -7,5 +7,6 @@ app_name = 'polls'
 
 #create a route and bring into main urls.py
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    path('<int:question_id>', views.detail, name="detail"),
 ]
--- pollster/polls/views.py
+++ pollster/polls/views.py
@@ -6,4 +6,18 @@ from .models import Question, Choice
 
 #Get questions and display them
 def index(request):
-    return render(request, 'polls/index.html')
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    context = {'latest_question_list': latest_question_list}
+    return render(request, 'polls/index.html', context)
+
+
+
+#Show specific questions and choices
+def detail(request, question_id):
+    try:
+        question = Question.objects.get(pk=question_id)
+    #If question id doesn't exist, raise an http 404
+    except Question.DoesNotExist:
+        raise Http404("Question does not exist")
+    #pass in question as a dict and load details
+    return render(request, 'polls/results.html', {'question': question})
